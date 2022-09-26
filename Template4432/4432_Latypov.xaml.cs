57342c79ba44e28bdd7ed4b50fc16be036df6548
damir <latypovdamir3@gmail.com>
2022-09-26 21:09:54 +0300

    succes to complete third laboratory work

--- Template4432/4432_Latypov.xaml.cs
+++ Template4432/4432_Latypov.xaml.cs
@@ -177,21 +177,103 @@ namespace Template4432
 
         private void ExportJson_Click(object sender, RoutedEventArgs e)
         {
-            List<Services> allServices;
-            using (SecondISRPOLabaEntities usersEntities = new SecondISRPOLabaEntities())
+            Task.Run(() => //async runtume - anti screen lock
             {
-                allServices = usersEntities.Services.ToList()
-                    .OrderBy(s => s.name)
-                    .ToList();
-            }
-            #region convenient grouping structure
-            var studentsCategories = allServices
-                    .OrderBy(o => o.cost)
-                    .GroupBy(s => s.cost)
-                    .ToDictionary(g => g.Key, g => g.Select(s => new { s.id, s.name, s.type, s.cost })
-                    .ToArray());
-            #endregion
+                List<Services> allServices;
+                using (SecondISRPOLabaEntities usersEntities = new SecondISRPOLabaEntities())
+                {
+                    allServices = usersEntities.Services.ToList()
+                        .OrderBy(s => s.name)
+                        .ToList();
+                }
 
+                #region convenient grouping structure
+                var costsCategories = allServices
+                        .OrderBy(o => o.cost)
+                        .GroupBy(s => s.cost)
+                        .ToDictionary(g => g.Key, g => g.Select(s => new { s.id, s.name, s.type, s.cost })
+                        .ToArray());
+                #endregion
+
+                var app = new Microsoft.Office.Interop.Word.Application();
+                Microsoft.Office.Interop.Word.Document document = app.Documents.Add();
+
+
+                for (int i = 0; i < _sheetsCount; i++)
+                {
+                    #region spliting by condition
+                    var data = i == 0 ? costsCategories.Where(w => w.Key.HasValue && w.Key.Value >= 0 && w.Key.Value <= 350)
+                        : i == 1 ? costsCategories.Where(w => w.Key.HasValue && w.Key.Value >= 250 && w.Key.Value <= 800)
+                        : i == 2 ? costsCategories.Where(w => w.Key.HasValue && w.Key.Value >= 800) : costsCategories;
+                    Microsoft.Office.Interop.Word.Paragraph paragraph = document.Paragraphs.Add();
+                    Microsoft.Office.Interop.Word.Range range = paragraph.Range;
+                    range.Text = $"Категория {i + 1}";
+                    paragraph.set_Style("Заголовок 1");
+                    range.InsertParagraphAfter();
+                    var tableParagraph = document.Paragraphs.Add();
+                    var tableRange = tableParagraph.Range;
+                    var studentsTable =
+                    document.Tables.Add(tableRange, data.Select(s => s.Value.Length).Sum() + 1, 4);
+                    studentsTable.Borders.InsideLineStyle =
+                    studentsTable.Borders.OutsideLineStyle =
+                    Microsoft.Office.Interop.Word.WdLineStyle.wdLineStyleSingle;
+                    studentsTable.Range.Cells.VerticalAlignment =
+                    Microsoft.Office.Interop.Word.WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+                    Microsoft.Office.Interop.Word.Range cellRange;
+                    cellRange = studentsTable.Cell(1, 1).Range;
+                    cellRange.Text = "Id";
+                    cellRange = studentsTable.Cell(1, 2).Range;
+                    cellRange.Text = "Название услуги";
+                    cellRange = studentsTable.Cell(1, 3).Range;
+                    cellRange.Text = "Вид услуги";
+                    cellRange = studentsTable.Cell(1, 4).Range;
+                    cellRange.Text = "Стоимость";
+                    studentsTable.Rows[1].Range.Bold = 1;
+                    studentsTable.Rows[1].Range.ParagraphFormat.Alignment =
+                    Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    int row = 1;
+                    var stepSize = 1;
+                    foreach (var group in data)
+                    {
+                        #endregion
+
+                        foreach (var currentCost in group.Value)
+                        {
+                            cellRange = studentsTable.Cell(row + stepSize, 1).Range;
+                            cellRange.Text = currentCost.id.ToString();
+                            cellRange.ParagraphFormat.Alignment =
+                            Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+
+                            cellRange = studentsTable.Cell(row + stepSize, 2).Range;
+                            cellRange.Text = currentCost.name;
+                            cellRange.ParagraphFormat.Alignment =
+                            Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+
+                            cellRange = studentsTable.Cell(row + stepSize, 3).Range;
+                            cellRange.Text = currentCost.type;
+                            cellRange.ParagraphFormat.Alignment =
+                            Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+
+                            cellRange = studentsTable.Cell(row + stepSize, 4).Range;
+                            cellRange.Text = currentCost.cost.ToString();
+                            cellRange.ParagraphFormat.Alignment =
+                            Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                            row++;
+                        }
+                    }
+                    Microsoft.Office.Interop.Word.Paragraph countCostsParagraph = document.Paragraphs.Add();
+                    Microsoft.Office.Interop.Word.Range countCostsRange = countCostsParagraph.Range;
+                    countCostsRange.Text = $"Данных в группировке - { data.Select(s => s.Value.Length).Sum()} ";
+                    countCostsRange.Font.Color = Microsoft.Office.Interop.Word.WdColor.wdColorDarkRed;
+                    countCostsRange.InsertParagraphAfter();
+                    document.Words.Last.InsertBreak(Microsoft.Office.Interop.Word.WdBreakType.wdSectionBreakNextPage);
+                }
+                app.Visible = true;
+                document.SaveAs2(@"D:\outputFileWord.docx");
+                document.SaveAs2(@"D:\outputFilePdf.pdf",
+                Microsoft.Office.Interop.Word.WdExportFormat.wdExportFormatPDF);
+            });
         }
     }
 }
+
