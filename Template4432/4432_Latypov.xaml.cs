28c8836d3219fe2bfebdca59a7a4223489eb4bb6
Ramil Rahimov <ramil.rahimov2003@gmail.com>
2022-10-09 23:36:00 +0300

    Реализована работа с эксель

--- Template4432/4432_Latypov.xaml.cs
+++ Template4432/4432_Latypov.xaml.cs
@@ -29,124 +29,124 @@ namespace Template4432
             InitializeComponent();
         }
 
-        private void Import_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog ofd = new OpenFileDialog()
-            {
-                DefaultExt = "*.xls;*.xlsx",
-                Filter = "файл Excel (Spisok.xlsx)|*.xlsx",
-                Title = "Выберите файл базы данных"
-            };
-            if (!(ofd.ShowDialog() == true))
-                return;
-            lock (ofd) // lock the thread against "crirtical section"
-            {
-                string[,] list;
+        //private void Import_Click(object sender, RoutedEventArgs e)
+        //{
+        //    OpenFileDialog ofd = new OpenFileDialog()
+        //    {
+        //        DefaultExt = "*.xls;*.xlsx",
+        //        Filter = "файл Excel (Spisok.xlsx)|*.xlsx",
+        //        Title = "Выберите файл базы данных"
+        //    };
+        //    if (!(ofd.ShowDialog() == true))
+        //        return;
+        //    lock (ofd) // lock the thread against "crirtical section"
+        //    {
+        //        string[,] list;
 
-                var ObjWorkExcel = new Excel.Application();
+        //        var ObjWorkExcel = new Excel.Application();
 
-                var ObjWorkBook = ObjWorkExcel.Workbooks.Open(ofd.FileName);
+        //        var ObjWorkBook = ObjWorkExcel.Workbooks.Open(ofd.FileName);
 
-                var ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1];
+        //        var ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1];
 
-                var lastCell = ObjWorkSheet
-                    .Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);
+        //        var lastCell = ObjWorkSheet
+        //            .Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);
 
-                var _columns = lastCell.Column;
+        //        var _columns = lastCell.Column;
 
-                var _rows = lastCell.Row;
+        //        var _rows = lastCell.Row;
 
-                list = new string[_rows, _columns];
+        //        list = new string[_rows, _columns];
 
-                for (int j = 0; j < _columns; j++)
-                    for (int i = 0; i < _rows; i++)
-                        list[i, j] = ObjWorkSheet.Cells[i + 1, j + 1].Text;
+        //        for (int j = 0; j < _columns; j++)
+        //            for (int i = 0; i < _rows; i++)
+        //                list[i, j] = ObjWorkSheet.Cells[i + 1, j + 1].Text;
 
-                ObjWorkBook.Close(false, Type.Missing, Type.Missing);
+        //        ObjWorkBook.Close(false, Type.Missing, Type.Missing);
 
-                ObjWorkExcel.Quit();
+        //        ObjWorkExcel.Quit();
 
-                GC.Collect();
-                using (SecondISRPOLabaEntities entities = new SecondISRPOLabaEntities())
-                {
-                    for (int i = 1; i < _rows; i++)
-                    {
-                        entities.Services.Add(new Services()
-                        {
-                            id = int.Parse(list[i, 0]),
-                            name = list[i, 1],
-                            type = list[i, 2],
-                            code = list[i, 3],
-                            cost = decimal.Parse(list[i, 4])
-                        });
-                    }
-                    entities.SaveChanges();
-                }
-            }
-        }
+        //        GC.Collect();
+        //        using (SecondISRPOLabaEntities entities = new SecondISRPOLabaEntities())
+        //        {
+        //            for (int i = 1; i < _rows; i++)
+        //            {
+        //                entities.Services.Add(new Services()
+        //                {
+        //                    id = int.Parse(list[i, 0]),
+        //                    name = list[i, 1],
+        //                    type = list[i, 2],
+        //                    code = list[i, 3],
+        //                    cost = decimal.Parse(list[i, 4])
+        //                });
+        //            }
+        //            entities.SaveChanges();
+        //        }
+        //    }
+        //}
 
-        private void Export_Click(object sender, RoutedEventArgs e)
-        {
-            Task.Run(() => //async runtume - anti screen lock
-            {
+        //private void Export_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Task.Run(() => //async runtume - anti screen lock
+        //    {
 
-                List<Services> allServices;
-                using (SecondISRPOLabaEntities usersEntities = new SecondISRPOLabaEntities())
-                {
-                    allServices = usersEntities.Services.ToList()
-                        .OrderBy(s => s.name)
-                        .ToList();
-                }
-                var app = new Excel.Application();
-                app.SheetsInNewWorkbook = _sheetsCount;
-                Excel.Workbook workbook = app.Workbooks.Add(Type.Missing);
-                #region convenient grouping structure
-                var studentsCategories = allServices
-                        .OrderBy(o => o.cost)
-                        .GroupBy(s => s.cost)
-                        .ToDictionary(g => g.Key, g => g.Select(s => new { s.id, s.name, s.type, s.cost })
-                        .ToArray());
-                #endregion
-                for (int i = 0; i < _sheetsCount; i++)
-                {
-                    int startRowIndex = 1;
-                    var worksheet = app.Worksheets.Item[i + 1];
-                    #region headers
-                    worksheet.Name = $"Категория {i + 1}";
-                    worksheet.Cells[1][startRowIndex] = "Id";
-                    worksheet.Cells[2][startRowIndex] = "Название услуги";
-                    worksheet.Cells[3][startRowIndex] = "Вид услуги";
-                    worksheet.Cells[4][startRowIndex] = "Стоимость";
-                    startRowIndex++;
-                    #endregion
+        //        List<Services> allServices;
+        //        using (SecondISRPOLabaEntities usersEntities = new SecondISRPOLabaEntities())
+        //        {
+        //            allServices = usersEntities.Services.ToList()
+        //                .OrderBy(s => s.name)
+        //                .ToList();
+        //        }
+        //        var app = new Excel.Application();
+        //        app.SheetsInNewWorkbook = _sheetsCount;
+        //        Excel.Workbook workbook = app.Workbooks.Add(Type.Missing);
+        //        #region convenient grouping structure
+        //        var studentsCategories = allServices
+        //                .OrderBy(o => o.cost)
+        //                .GroupBy(s => s.cost)
+        //                .ToDictionary(g => g.Key, g => g.Select(s => new { s.id, s.name, s.type, s.cost })
+        //                .ToArray());
+        //        #endregion
+        //        for (int i = 0; i < _sheetsCount; i++)
+        //        {
+        //            int startRowIndex = 1;
+        //            var worksheet = app.Worksheets.Item[i + 1];
+        //            #region headers
+        //            worksheet.Name = $"Категория {i + 1}";
+        //            worksheet.Cells[1][startRowIndex] = "Id";
+        //            worksheet.Cells[2][startRowIndex] = "Название услуги";
+        //            worksheet.Cells[3][startRowIndex] = "Вид услуги";
+        //            worksheet.Cells[4][startRowIndex] = "Стоимость";
+        //            startRowIndex++;
+        //            #endregion
 
-                    #region spliting by condition
-                    var data = i == 0 ? studentsCategories.Where(w => w.Key.HasValue && w.Key.Value >= 0 && w.Key.Value <= 350)
-                        : i == 1 ? studentsCategories.Where(w => w.Key.HasValue && w.Key.Value >= 250 && w.Key.Value <= 800)
-                        : i == 2 ? studentsCategories.Where(w => w.Key.HasValue && w.Key.Value >= 800) : studentsCategories;
-                    #endregion
+        //            #region spliting by condition
+        //            var data = i == 0 ? studentsCategories.Where(w => w.Key.HasValue && w.Key.Value >= 0 && w.Key.Value <= 350)
+        //                : i == 1 ? studentsCategories.Where(w => w.Key.HasValue && w.Key.Value >= 250 && w.Key.Value <= 800)
+        //                : i == 2 ? studentsCategories.Where(w => w.Key.HasValue && w.Key.Value >= 800) : studentsCategories;
+        //            #endregion
 
-                    foreach (var students in data)
-                    {
-                        foreach (var student in students.Value)
-                        {
-                            if (student.cost == students.Key)
-                            {
-                                #region fill the fields
-                                worksheet.Cells[1][startRowIndex] = student.id;
-                                worksheet.Cells[2][startRowIndex] = student.name;
-                                worksheet.Cells[3][startRowIndex] = student.type;
-                                worksheet.Cells[4][startRowIndex] = student.cost;
-                                startRowIndex++;
-                                #endregion
-                            }
-                        }
-                    }
-                    worksheet.Columns.AutoFit();
-                }
-                app.Visible = true;
-            });
+        //            foreach (var students in data)
+        //            {
+        //                foreach (var student in students.Value)
+        //                {
+        //                    if (student.cost == students.Key)
+        //                    {
+        //                        #region fill the fields
+        //                        worksheet.Cells[1][startRowIndex] = student.id;
+        //                        worksheet.Cells[2][startRowIndex] = student.name;
+        //                        worksheet.Cells[3][startRowIndex] = student.type;
+        //                        worksheet.Cells[4][startRowIndex] = student.cost;
+        //                        startRowIndex++;
+        //                        #endregion
+        //                    }
+        //                }
+        //            }
+        //            worksheet.Columns.AutoFit();
+        //        }
+        //        app.Visible = true;
+        //    });
 
-        }
+        //}
     }
 }
--- Template4432/4432_RakhimovRamil.xaml.cs
+++ Template4432/4432_RakhimovRamil.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Template4432
 {
@@ -22,6 +24,113 @@ namespace Template4432
         public _4432_RakhimovRamil()
         {
             InitializeComponent();
+        }
+
+        private void Button_Import_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog()
+            {
+                DefaultExt = "*.xls;*.xlsx",
+                Filter = "файл Excel (Spisok.xlsx)|*.xlsx",
+                Title = "Выберите файл базы данных"
+            };
+            if (!(ofd.ShowDialog() == true))
+                return;
+            string[,] list;
+
+            Excel.Application ObjWorkExcel = new Excel.Application();
+
+            Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(ofd.FileName);
+
+            Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1];
+
+            var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);
+            int _columns = (int)lastCell.Column;
+            int _rows = (int)lastCell.Row;
+            list = new string[_rows, _columns];
+            for (int j = 0; j < _columns; j++)
+                for (int i = 0; i < _rows; i++)
+                    list[i, j] = ObjWorkSheet.Cells[i + 1, j + 1].Text;
+            ObjWorkBook.Close(false, Type.Missing, Type.Missing);
+            ObjWorkExcel.Quit();
+            GC.Collect();
+
+            using(var db = new ISRPODBEntities())
+            {
+                for (int i = 1; i < _rows; i++)
+                {
+                    int nullCollumns = 0;
+                    for (int j = 0; j < _columns; j++)
+                    {
+                        if (String.IsNullOrEmpty(list[i, j]))
+                            nullCollumns++;
+                    }
+                    if (nullCollumns == _columns)
+                        continue;
+                    db.Client.Add(new Client()
+                    {
+                        FIO = list[i, 0],
+                        UserId = Convert.ToInt32(list[i, 1]),
+                        BirthDate = list[i, 2],
+                        Index = Convert.ToInt32(list[i, 3]),
+                        City = list[i, 4],
+                        Street = list[i, 5],           
+                        House = Convert.ToInt32(list[i, 6]),
+                        Apartment = Convert.ToInt32(list[i, 7]),
+                        Email = list[i, 8]
+                    }); 
+                }
+                db.SaveChanges();
+            }
+        }
+
+        private void Button_Export_Click(object sender, RoutedEventArgs e)
+        {
+            List<Client> allClients;
+            List<string> allStreets;
+            using (var isrpoEntities = new ISRPODBEntities())
+            {
+                allClients = (from c in isrpoEntities.Client
+                              orderby c.FIO
+                              select c).ToList();
+                allStreets = (from s in allClients
+                              group s by s.Street into g
+                              select g.Key).ToList();
+            }
+            var app = new Excel.Application();
+            app.SheetsInNewWorkbook = allStreets.Count;
+            Excel.Workbook workbook = app.Workbooks.Add(Type.Missing);
+
+            for (int i = 0; i < allStreets.Count; i++)
+            {
+                int startRowIndex = 1;
+                Excel.Worksheet worksheet = app.Worksheets.Item[i + 1];
+                worksheet.Name = allStreets[i];
+                worksheet.Cells[1][startRowIndex] = "Код клиента";
+                worksheet.Cells[2][startRowIndex] = "ФИО";
+                worksheet.Cells[3][startRowIndex] = "E-mail";
+                Excel.Range headerRange = worksheet.Range[worksheet.Cells[1][1], worksheet.Cells[3][1]];
+                headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                headerRange.Font.Bold = true;
+                startRowIndex++;
+                foreach(Client client in allClients)
+                {
+                    if (client.Street != allStreets[i])
+                        continue;
+                    worksheet.Cells[1][startRowIndex] = client.UserId;
+                    worksheet.Cells[2][startRowIndex] = client.FIO;
+                    worksheet.Cells[3][startRowIndex] = client.Email;
+                    startRowIndex++;
+                }
+                Excel.Range rangeBorders = worksheet.Range[worksheet.Cells[1][1], worksheet.Cells[3][startRowIndex - 1]];             
+                rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeBottom].LineStyle =
+                rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeLeft].LineStyle =
+                rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeTop].LineStyle =
+                rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeRight].LineStyle =
+                rangeBorders.Borders[Excel.XlBordersIndex.xlInsideHorizontal].LineStyle = rangeBorders.Borders[Excel.XlBordersIndex.xlInsideVertical].LineStyle = Excel.XlLineStyle.xlContinuous;
+                worksheet.Columns.AutoFit();
+            }
+            app.Visible = true;
         }
     }
 }
