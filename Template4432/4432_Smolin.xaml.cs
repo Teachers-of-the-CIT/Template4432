d7e1d6386ff9bbb3de9ccd77e46030974b1a685a
aleksandr <smalex2003@mail.ru>
2022-10-18 18:30:21 +0300

    Complete 3 lab

--- Template4432/4432_Smolin.xaml.cs
+++ Template4432/4432_Smolin.xaml.cs
@@ -15,7 +15,8 @@ using Microsoft.Win32;
 using System.IO;
 using Word = Microsoft.Office.Interop.Word;
 using Excel = Microsoft.Office.Interop.Excel;
-
+using System.Text.Json;
+using Microsoft.Office.Interop.Word;
 
 namespace Template4432
 {
@@ -23,9 +24,10 @@ namespace Template4432
     /// <summary>
     /// Логика взаимодействия для _4432_Smolin.xaml
     /// </summary>
-    public partial class _4432_Smolin : Window
+    public partial class _4432_Smolin : System.Windows.Window
     {
         LR2IsrpoEntities db = new LR2IsrpoEntities();
+
         public _4432_Smolin()
         {
             InitializeComponent();
@@ -135,5 +137,153 @@ namespace Template4432
             }
             app.Visible = true;
         }
+
+        public class Worker1
+        {
+            public string CodeStaff { get; set; }
+            public string Position { get; set; }
+            public string FullName { get; set; }
+            public string Log { get; set; }
+            public string Password { get; set; }
+            public string LastEnter { get; set; }
+            public string TypeEnter { get; set; }
+        }
+
+        private void SmolinImport2_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                FileStream fs = new FileStream(ofd.FileName, FileMode.Open);
+                List<Worker1> personlist = JsonSerializer.Deserialize<List<Worker1>>(fs);
+                foreach (Worker1 person in personlist)
+                {
+                    Worker work = new Worker();
+                    work.WorkerID = int.Parse(person.CodeStaff.Remove(0, 3));
+                    work.Post = person.Position;
+                    work.Password = person.Password;
+                    work.Last_authorization = person.LastEnter;
+                    work.Type_authorization = person.TypeEnter;
+                    work.Full_name = person.FullName;
+                    work.Login = person.Log;
+                    db.Worker.Add(work);
+                  
+                    
+                }
+                try
+                {
+                    db.SaveChanges();
+                    MessageBox.Show("Данные успешно импортированны");
+                }
+                catch
+                {
+
+                    MessageBox.Show("Произошла ошибка");
+                }
+              
+            }
+        }
+
+        private void SmolinExport2_Click(object sender, RoutedEventArgs e)
+        {
+            List<Worker> employees = db.Worker.ToList().OrderBy(p => p.Type_authorization).ToList();
+            var app = new Word.Application();
+            Word.Document doc = app.Documents.Add();
+            Word.Paragraph paragraph =
+            doc.Paragraphs.Add();
+            Word.Range range = paragraph.Range;
+            paragraph.set_Style("Заголовок 1");
+            range.InsertParagraphAfter();
+
+            var employeeTypeSuccess = employees.Where(p => p.Type_authorization == "Успешно").ToList();
+            var employeeTypeFail = employees.Where(p => p.Type_authorization == "Неуспешно").ToList();
+            int Seller1Count= employeeTypeSuccess.Where(p => p.Post == "Продавец").ToList().Count;
+            int Seller2Count = employeeTypeFail.Where(p => p.Post == "Продавец").ToList().Count;
+            Word.Paragraph table = doc.Paragraphs.Add();
+            Word.Range tablerange = table.Range;
+            Word.Table employeetable = doc.Tables.Add(tablerange, employeeTypeSuccess.Count + 1, 3);
+            employeetable.Borders.InsideLineStyle = employeetable.Borders.OutsideLineStyle = Word.WdLineStyle.wdLineStyleSingle;
+            employeetable.Range.Cells.VerticalAlignment = WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+
+            Word.Range cellRange;
+            cellRange = employeetable.Cell(1, 1).Range;
+            cellRange.Text = "Код";
+            cellRange = employeetable.Cell(1, 2).Range;
+            cellRange.Text = "Должность";
+            cellRange = employeetable.Cell(1, 3).Range;
+            cellRange.Text = "Логин";
+            employeetable.Rows[1].Range.Bold = 1;
+            employeetable.Rows[1].Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            //Заполнение
+            int i = 1;
+            foreach (var item in employeeTypeSuccess)
+            {
+                cellRange = employeetable.Cell(i + 1, 1).Range;
+                cellRange.Text = item.WorkerID.ToString();
+                cellRange.ParagraphFormat.Alignment =
+                Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                cellRange = employeetable.Cell(i + 1, 2).Range;
+                cellRange.Text = item.Post;
+                cellRange.ParagraphFormat.Alignment =
+                Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                cellRange = employeetable.Cell(i + 1, 3).Range;
+                cellRange.Text = item.Login;
+                cellRange.ParagraphFormat.Alignment =
+                Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                i++;
+            }
+
+            Word.Paragraph par = doc.Paragraphs.Add();
+            Word.Range ran = par.Range;
+            ran.Text = "Всего продавцов с успешным входом: " + Seller1Count;
+            ran.InsertParagraphAfter();
+            ran.Font.Size = 26;
+            doc.Words.Last.InsertBreak(Word.WdBreakType.wdPageBreak);
+
+            Word.Paragraph table2 = doc.Paragraphs.Add();
+            Word.Range tablerange2 = table2.Range;
+            Word.Table employeetable2 = doc.Tables.Add(tablerange2, employeeTypeFail.Count + 1, 3);
+            employeetable2.Borders.InsideLineStyle = employeetable2.Borders.OutsideLineStyle = Word.WdLineStyle.wdLineStyleSingle;
+            employeetable2.Range.Cells.VerticalAlignment = WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+
+            Word.Range cellRange2;
+            cellRange2 = employeetable2.Cell(1, 1).Range;
+            cellRange2.Text = "Код";
+            cellRange2 = employeetable2.Cell(1, 2).Range;
+            cellRange2.Text = "Должность";
+            cellRange2 = employeetable2.Cell(1, 3).Range;
+            cellRange2.Text = "Логин";
+            employeetable2.Rows[1].Range.Bold = 1;
+            employeetable2.Rows[1].Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            //Заполнение
+            i = 1;
+            foreach (var item in employeeTypeFail)
+            {
+                cellRange2 = employeetable2.Cell(i + 1, 1).Range;
+                cellRange2.Text = item.WorkerID.ToString();
+                cellRange2.ParagraphFormat.Alignment =
+                Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                cellRange2 = employeetable2.Cell(i + 1, 2).Range;
+                cellRange2.Text = item.Post;
+                cellRange2.ParagraphFormat.Alignment =
+                Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                cellRange2 = employeetable2.Cell(i + 1, 3).Range;
+                cellRange2.Text = item.Login;
+                cellRange2.ParagraphFormat.Alignment =
+                Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                i++;
+            }
+
+            Word.Paragraph par2 = doc.Paragraphs.Add();
+            Word.Range ran2 = par2.Range;
+            ran2.Text = "Всего продавцов с неуспешным входом: " + Seller2Count;
+            ran2.InsertParagraphAfter();
+            ran2.Font.Size = 26;
+            doc.Words.Last.InsertBreak(Word.WdBreakType.wdPageBreak);
+            app.Visible = true;
+
+        }
+
+       
     }
 }
